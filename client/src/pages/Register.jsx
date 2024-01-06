b5144348a5a85e31a525ac7166b147555d5615ef
Akshay <akshaykhatri22@gmail.com>
2024-01-06 18:20:51 +0530

    Register Page Added

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow } from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form action="" className="form">
+        <Logo />
+        <h4>Register</h4>
+
+        <FormRow type="text" name="name" defaultValue="Akshay" />
+        <FormRow
+          type="text"
+          name="lastname"
+          labelText="Last Name"
+          defaultValue="Khatri"
+        />
+        <FormRow type="text" name="location" defaultValue="India" />
+        <FormRow
+          type="email"
+          name="email"
+          defaultValue="akshaykhatri22@gmail.com"
+        />
+        <FormRow type="password" name="password" defaultValue="secret123" />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
