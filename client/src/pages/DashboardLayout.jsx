2f2cec00f30aa8454bf5fd1572e0bd560d352bbf
Akshay Khatri <akshay.khatri@cognisun.com>
2024-01-08 15:59:16 +0530

    Dark Theme Functionality added

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useState, createContext } from "react";
 import { Outlet } from "react-router-dom";
 import Wrapper from "../assets/wrappers/Dashboard";
 import { BigSidebar, Navbar, SmallSidebar } from "../components";
+import { checkDefaultTheme } from "../App";
 
 const DashboardContext = createContext();
 
@@ -9,9 +10,13 @@ const DashboardLayout = () => {
   // temp
   const user = { name: "akshay" };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
   const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
     console.log("toggle dark theme");
   };
 
