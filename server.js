9c771108dc2b9bf50670569fecdb758e931a5cd8
Akshay <akshaykhatri22@gmail.com>
2024-01-15 15:17:12 +0530

    Morgan and dotenv setup

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-import express from 'express'
-const app = express()
+import * as dotenv from "dotenv";
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+import express from "express";
+const app = express();
+import morgan from "morgan";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-app.listen(5100, () => {
-    console.log(`Server is running on http://localhost:5100`)
-})
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server running on PORT ${port} http://localhost:5100`);
+});
