8a67cb7baa6de20f2f8ee9e912759f2cb4a1a856
Akshay <akshaykhatri22@gmail.com>
2024-01-21 23:28:10 +0530

    Custom Error Middleware Setup

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,6 @@
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
-
-import { nanoid } from "nanoid";
-
-let jobs = [
-  { id: nanoid(), company: "SMX Exports", position: "front-end" },
-  { id: nanoid(), company: "Alpine Textile", position: "back-end" },
-];
+import { NotFoundError } from "../errors/customError.js";
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
@@ -21,9 +15,7 @@ export const createJob = async (req, res) => {
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
-  if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!job) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ job });
 };
 
@@ -32,17 +24,13 @@ export const updateJob = async (req, res) => {
   const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
     new: true,
   });
-  if (!updatedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!updatedJob) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ msg: "job modified", job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findByIdAndDelete(id);
-  if (!removedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
-  }
+  if (!removedJob) throw new NotFoundError(`no job with id ${id}`);
   res.status(StatusCodes.OK).json({ msg: "job deleted" });
 };
