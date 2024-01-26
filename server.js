3bb456f8650e96ecc675f19a457196f222185203
Akshay <akshaykhatri22@gmail.com>
2024-01-26 17:01:54 +0530

    Current User Get and Update Route

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import cookieParser from "cookie-parser";
 // routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 
 // middlewares
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -27,8 +28,9 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
