d02f87844cefdb00fcf6546b51f7885ba5d2389f
Akshay <akshaykhatri22@gmail.com>
2024-02-21 23:35:03 +0530

    Test User Setup for Explore the app

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Link, Form, redirect, useActionData } from "react-router-dom";
+import {
+  Link,
+  Form,
+  redirect,
+  useActionData,
+  useNavigate,
+} from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo, SubmitBtn } from "../components";
 import customFetch from "../utils/customFetch";
@@ -28,6 +34,20 @@ export const action = async ({ request }) => {
 
 const Login = () => {
   const errors = useActionData();
+  const navigate = useNavigate();
+  const loginDemoUser = async () => {
+    const data = {
+      email: "test@test.com",
+      password: "secret123",
+    };
+    try {
+      await customFetch.post("/auth/login", data);
+      toast.success("Take a test drive");
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
 
   return (
     <Wrapper>
@@ -44,7 +64,7 @@ const Login = () => {
         />
         <FormRow type="password" name="password" defaultValue="secret123" />
         <SubmitBtn />
-        <button type="button" className="btn btn-block">
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
           explore the app
         </button>
         <p>
