c4b2ffb8652a98a380a5e38bc6549b7ac838d22f
Akshay <akshaykhatri22@gmail.com>
2024-02-23 00:54:27 +0530

    Bar Chart Area Chart Button Toggler

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
-const ChartsContainer = () => {
-  return <h1>ChartsContainer</h1>;
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import Wrapper from "../assets/wrappers/ChartsContainer";
+
+const ChartsContainer = ({ data }) => {
+  const [barChart, setBarChart] = useState(true);
+  return (
+    <Wrapper>
+      <h4>Monthly Application</h4>
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "Area Chart" : "Bar Chart"}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
