022b7132712b8dc731db08f6b8898f7e1a9becbb
Akshay <akshaykhatri22@gmail.com>
2024-02-24 17:18:11 +0530

    Loader and Query Params

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -6,9 +6,14 @@ import customFetch from "../utils/customFetch";
 import { useLoaderData } from "react-router-dom";
 import { useContext, createContext } from "react";
 
-export const loader = async () => {
+export const loader = async ({ request }) => {
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
   try {
-    const { data } = await customFetch.get("/jobs");
+    const { data } = await customFetch.get("/jobs", {
+      params,
+    });
     return { data };
   } catch (error) {
     toast.error(error?.response?.data?.msg);
